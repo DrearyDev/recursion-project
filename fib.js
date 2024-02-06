c89f917a0f47f3995d597520253f91800482b6f0
drearydev <143649090+DrearyDev@users.noreply.github.com>
2024-02-06 14:19:28 -0500

    Add explination to fib.js recursive function

--- fib.js
+++ fib.js
@@ -39,3 +39,34 @@ function fibsRecursion(n) {
     return arr;
 };
 console.log(fibsRecursion(8));// [0, 1, 1, 2, 3, 5, 8, 13]
+
+/*------------------------------------------------------------
+    Whats Happening in this Recursive Function
+
+fibsRecursion(8)
+    arr = fibsRecursion(7)
+        arr = fibsRecursion(6)
+            arr = fibsRecursion(5)
+                arr = fibsRecursion(4)
+                    arr = fibsRecursion(3)
+                        arr = fibsRecursion(2)
+                            arr = fibsRecursion(1)
+                                return [0, 1]
+                        return [0, 1, 1]
+                    return [0, 1, 1, 2]
+                return [0, 1, 1, 2, 3]
+            return [0, 1, 1, 2, 3, 5]
+        return [0, 1, 1, 2, 3, 5, 8]
+    return [0, 1, 1, 2, 3, 5, 8, 13]
+
+this function sets an array to the function called with the number passed minus 1.
+this happens all the way until we reach fibsRecursion(1) which returns [0, 1] before
+it ever calls fibsRecursion again. now that we have an array, we can do the math that
+pushes a new number to the array. this math is adding the two previous elements to get the
+new element (which is how you calculate the fibonacci sequence). It then returns the array
+it made and does this over and over until we are back at our first call, but with the created
+array where we can add the two previous elements to get the last element.
+
+this function returns the fibonacci sequence as an array of length 'n' where n is what you pass in.
+
+------------------------------------------------------------*/
